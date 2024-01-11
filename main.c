5f7fcdbbc0b6d5b4c134cbdbdfafc3d5927653d4
Meryem Arpacı <meryemarpaci8@gmail.com>
2024-01-11 23:39:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,31 +2,6 @@
 #include <stdlib.h>
 #include "sos.h"
 
-
-/* 
-#define MB 10485760
-#define MEM 100*MB
-	void *ptr = NULL;
-	while(ptr=malloc(MEM)) {
-		memset(ptr,0,MEM);
-	}
-	
-ekrana baklava þekli çiz
-ekrana dik üçgen çiz
-enigma þifrelemeyi yap
-basit bir sos oyunu tasarla
-
-3x3 lük tahta %-20s ile göster
-
-Ý için 111 222 333
-j için 111 222 333
-capraz ij-1 ve ij-2 ve ij-3
-digeri ij-3 ve ij-2 ve ij1 
-sýra sýra eleman al yukardaki koþullarý saðlayan kazanýr
-
-*/
-
-
 int main() {
 	
 	baslama();
